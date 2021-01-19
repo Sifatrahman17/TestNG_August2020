7a0c1e68bb82e7adbd9c3f47f6737ae8b615ee0c
Sifatali17 <Sifatrahman17@gmail.com>
2021-01-19 16:40:29 -0600

    commit2

---
+++ src/main/java/VAriousConcept/GitTest.java
@@ -0,0 +1,14 @@
+package VAriousConcept;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void printMe() {
+		System.out.println("Automation testing with Git");
+	}
+
+}
